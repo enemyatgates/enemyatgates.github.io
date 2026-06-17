c363c752fdb933ef06f6a7574a97ea41426a7e82
enemyatgates <enemy.at.gates@gmail.com>
2026-06-18 04:03:16 +0530

    Commit 2026.06.17 22:33:16 UTC

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import mdx from '@astrojs/mdx';
+
 export default defineConfig({
   site: 'https://enemyatgates.github.io',
 
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [mdx()],
 });
